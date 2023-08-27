d577982a9e0824cf31a588d5e3f37ca84e73a12a
vedant-11 <v.vedantsingh.11@gmail.com>
2023-08-28 01:21:20 +0530

    added code for routes and sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Sidebar from "./components/shared/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Activities from "./pages/activities";
+import Alert from "./pages/alert";
+import Dashboard from "./pages/dashboard";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Sidebar />
+      <Routes>
+        <Route path="/" component={Dashboard} />
+        <Route path="/activities" component={Activities} />
+        <Route path="/alert" component={Alert} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/shared/Sidebar.jsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  const manna = "yashshri";
+  return (
+    <>
+      <div className={manna}>
+        <p>hi</p>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default Sidebar;
