7624587ca820d1b10278b318e6530481caf3c4d0
Yashshri <yashashrijadhav2018@gmail.com>
2023-08-28 12:12:06 +0530

    Seperate files for shared components are created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Sidebar from "./components/shared/Sidebar";
+import {LSidebar} from "./components/shared/LSidebar";
 import { Routes, Route } from "react-router-dom";
 import Activities from "./pages/activities";
 import Alert from "./pages/alert";
@@ -7,7 +7,7 @@ import Dashboard from "./pages/dashboard";
 function App() {
   return (
     <div>
-      <Sidebar />
+      <LSidebar />
       <Routes>
         <Route path="/" component={Dashboard} />
         <Route path="/activities" component={Activities} />
---
+++ client/src/components/shared/Lsidebar.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+export const LSidebar = () => {
+  return (
+    <div>LSidebar</div>
+  )
+}
